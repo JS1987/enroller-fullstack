97a34ecb6cb02b9a8cfefd2e9cc45ccb6dd91c36
Jacek <jacek.sandomierski@gmail.com>
2018-06-09 11:55:40 +0200

    security

--- src/main/java/com/company/enroller/App.java
+++ src/main/java/com/company/enroller/App.java
@@ -1,7 +1,11 @@
 package com.company.enroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class App {
@@ -9,4 +13,12 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
 }
